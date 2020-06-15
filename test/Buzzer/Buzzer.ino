d9c3279733dd19c016cf1d548d33d32240009455
Noppamas045 <noppamas.sang@mail.kmutt.ac.th>
2020-06-15 17:06:53 +0700

    keypad+Buzzer

---
+++ test/Buzzer/Buzzer.ino
@@ -0,0 +1,293 @@
+void setup(){
+     pinMode(8,OUTPUT);
+    Serial.begin(9600);
+
+}
+
+void loop(){
+    tone(8,262,500);
+    delay(300);
+    tone(8,294,500);
+    delay(300);
+    tone(8,330,500);
+    delay(300);
+    tone(8,349,500);
+    delay(300);
+    tone(8,392,500);
+    delay(300);
+    tone(8,440,500);
+    delay(300);
+    tone(8,494,500);
+    delay(300);
+    tone(8,1047,500);
+    delay(300);
+
+
+
+
+
+
+
+
+
+
+//     Serial.println("_____Wu Ji_____");
+//     delay(5000);
+    
+//       tone(8, 880, 2000);
+//       //Serial.println("A5-");
+//       delay(1000);
+       
+//       tone(8, 987.8, 1000);
+//         //Serial.println("B5");
+//       delay(500);
+       
+//       tone(8, 784, 1000);
+//         //Serial.println("G5");
+//       delay(500);
+      
+//       tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(500);
+//             tone(8, 740, 1000);
+//        //Serial.println("F5#");
+//       delay(500);
+//             tone(8, 587.3, 1000);
+//        //Serial.println("D5");
+//       delay(500);
+//             tone(8, 483.9, 1000);
+//        //Serial.println("B4");
+//       delay(500);
+//             tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(1000);
+//             tone(8,740, 1000);
+//        //Serial.println("F5#");
+//       delay(500);
+//             tone(8, 784, 1000);
+//        //Serial.println("G5");
+//       delay(500);
+//             tone(8,880, 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//             tone(8,587.3, 1000);
+//        //Serial.println("D5");
+//       delay(500);
+//             tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(1000);
+//             tone(8, 880, 2000);
+//        //Serial.println("A5-");
+//       delay(500);
+//             tone(8, 987.8, 1000);
+//        //Serial.println("B5");
+//       delay(500);
+//             tone(8, 784, 1000);
+//        //Serial.println("G5");
+//       delay(500);
+//             tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(500);
+//             tone(8,740, 1000);
+//        //Serial.println("F5#");
+//       delay(500);
+//             tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(500);
+//             tone(8,587.3, 1000);
+//        //Serial.println("D5");
+//       delay(500);
+//             tone(8,523.3, 1000);
+//        //Serial.println("C5");
+//       delay(500);
+//             tone(8, 587.3, 1000);
+//        //Serial.println("D5");
+//       delay(500);
+//             tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(500);
+//             tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(500);
+//             tone(8, 880, 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//             tone(8,784, 1000);
+//        //Serial.println("G5");
+//       delay(500);
+//             tone(8,740, 1000);
+//        //Serial.println("F5#");
+//       delay(500);
+//             tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(500);
+//       tone(8, 740, 5000);
+//        //Serial.println("F5#");
+//       delay(1000);
+
+//     tone(8, 784, 1000);
+//        //Serial.println("G5");
+//       delay(500);
+//             tone(8, 880, 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//             tone(8, 987.8 , 1000);
+//        //Serial.println("B5");
+//       delay(500);
+//             tone(8, 880, 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//             tone(8, 784, 1000);
+//        //Serial.println("G5");
+//       delay(500);
+//             tone(8, 740, 1000);
+//        //Serial.println("F5#");
+//       delay(500);
+//             tone(8, 784, 1000);
+//        //Serial.println("G5");
+//       delay(500);
+//             tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(500);
+//         tone(8, 740, 1000);
+//        //Serial.println("F5#");
+//       delay(500);
+//                   tone(8, 1174.7, 2000);
+//        //Serial.println("D6-");
+//       delay(500);
+//                   tone(8,1318.5, 1000);
+//        //Serial.println("E6");
+//       delay(500);
+//                   tone(8, 1174.7, 1000);
+//        //Serial.println("D6");
+//       delay(500);
+
+//      tone(8, 784, 1000);
+//        //Serial.println("G5");
+//       delay(500);
+//         tone(8, 880, 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//       tone(8, 987.8 , 1000);
+//        //Serial.println("B5");
+//       delay(500);
+//       tone(8, 1046.5, 1000);
+//        //Serial.println("C6");
+//       delay(500);
+//               tone(8, 880, 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//             tone(8, 987.8 , 1000);
+//        //Serial.println("B5");
+//       delay(500);
+
+//             tone(8, 880 , 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//       tone(8, 987.8 , 1000);
+//        //Serial.println("B5");
+//       delay(500);
+//       tone(8, 1046.5, 2000);
+//        //Serial.println("C6-");
+//       delay(500);
+//       tone(8, 1174.7, 1000);
+//        //Serial.println("D6");
+//       delay(500);
+//       tone(8, 784, 1000);
+//        //Serial.println("G5");
+//       delay(500);
+
+//       tone(8, 740, 1000);
+//        //Serial.println("F5#");
+//       delay(500);
+//       tone(8, 784 , 1000);
+//        //Serial.println("G5");
+//       delay(500);
+//       tone(8, 880 , 2000);
+//        //Serial.println("A5-");
+//       delay(500);
+//       tone(8, 987.8 , 1000);
+//        //Serial.println("B5");
+//       delay(500);
+//       tone(8, 659.3, 1000);
+//        //Serial.println("E5");
+//       delay(500);
+
+//       tone(8, 587.3, 1000);
+//        //Serial.println("D5");
+//       delay(500);
+//       tone(8, 1174.7, 1000);
+//        //Serial.println("D6");
+//       delay(500);
+//       tone(8, 987.8 , 1000);
+//        //Serial.println("B5");
+//       delay(500);
+//       tone(8, 784 , 1000);
+//        //Serial.println("G5");
+//       delay(500);
+//       tone(8, 880, 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//       tone(8, 987.8 , 1000);
+//        //Serial.println("B5");
+//       delay(500);
+//       tone(8, 880, 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//       tone(8, 987.8 , 1000);
+//        //Serial.println("B5");
+//       delay(500);
+//       tone(8, 1174.7, 1000);
+//        //Serial.println("D6");
+//       delay(500);
+//       tone(8, 1318.5, 5000);
+//        //Serial.println("E6");
+//       delay(1000);
+
+
+//       tone(8, 880, 1000);
+//        //Serial.println("A5");
+//       delay(500);
+//       tone(8, 987.8 , 1000);
+//        //Serial.println("B5");
+//       delay(500);
+//       tone(8, 1174.7, 1000);
+//        //Serial.println("D6");
+//       delay(500);
+//       tone(8, 1568, 1000);
+//        //Serial.println("G6");
+//       delay(500);
+//       tone(8, 1480, 2000);
+//        //Serial.println("F6#-");
+//       delay(500);
+//       tone(8, 1396.9, 2000);
+//        //Serial.println("F6-");
+//       delay(500);
+//       tone(8, 1318.5, 2000);
+//        //Serial.println("E6");
+//       delay(500);
+
+//       tone(8, 1318.5, 1000);
+//        //Serial.println("E6");
+//       delay(500);
+//       tone(8, 1174.7, 1000);
+//        //Serial.println("D6");
+//       delay(500);
+//       tone(8, 1318.5, 1000);
+//        //Serial.println("E6");
+//       delay(500);
+//       tone(8, 1480, 1000);
+//        //Serial.println("F6#");
+//       delay(500);
+//       tone(8, 1174.7, 1000);
+//        //Serial.println("D6");
+//       delay(500);
+//             tone(8, 1318.5, 1000);
+//        //Serial.println("E6");
+//       delay(1000);
+
+//     Serial.println("END");
+//       delay(100000);
+     
+
+}
