0a42e0e2a56a9eaaa2e66a20aa28cc614012098a
Noppamas <61378261+Noppamas045@users.noreply.github.com>
2020-06-15 08:15:39 +0700

    edit

--- test/nextiontest1/nextiontest1.ino
+++ test/nextiontest1/nextiontest1.ino
@@ -1,5 +1,5 @@
 int pin = 3;
-int val ;
+int SC ;
 void setup(){
 Serial.begin(9600);
 pinMode(pin,INPUT);
@@ -7,15 +7,15 @@ pinMode(pin,INPUT);
 
 void loop(){
 
-val = digitalRead(pin);
-val = 1;
+SC = digitalRead(pin);
+SC = 1;
     Serial.print("page 1");
     Serial.write(0xff);
     Serial.write(0xff);
     Serial.write(0xff);
 delay(2000);
-val = digitalRead(pin);
-val = 0;
+SC = digitalRead(pin);
+SC = 0;
     Serial.print("page 0");
     Serial.write(0xff);
     Serial.write(0xff);
